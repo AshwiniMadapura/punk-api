0e2aee7f99256c2ee045b40e4bba5f8bbf9646be
AshwiniMadapura <ashwinidivakar8@gmail.com>
2022-04-20 16:00:27 +0100

    Created a data file

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import Employee from "./components/Employee/employee";
+import Nav from "./components/Employee/NavBar/Nav";
 
 const App = () => {
   return (
     <div>
       <p>Your cleaned App component</p>
+      <Employee/>
+      <Nav/>
     </div>
   );
 };
---
+++ src/components/Employee/employee.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Employee = () => {
+  return (
+    <div>CharacterCard</div>
+  )
+}
+
+export default Employee;
