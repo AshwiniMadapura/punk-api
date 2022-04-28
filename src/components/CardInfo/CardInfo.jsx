663a7d359c6b616479189c958280129335cb72ae
ashwini <ashwinidivakar8@gmail.com>
2022-04-28 15:10:15 +0100

    deleted unwanted files

--- src/components/CardInfo/CardInfo.jsx
+++ src/components/CardInfo/CardInfo.jsx
@@ -18,14 +18,14 @@ const CardInfo = ({ beerArray }) => {
                     <img className='image__section' src={list.image_url} alt="" />
                   </div>
                   <div className='details'>
-                  <h2 >Name: {list.name}</h2>
-                  <h4>{list.tagline}</h4>
-                  <h4>First brewed on:{list.first_brewed}</h4>
-                  <h4>ID: {list.id}</h4>
-                  <p className='side_heading'>Description: {list.description}</p><br />
-                  <h4>Preparation</h4>
+                  <h1 >{list.name}</h1>
+                  <h4>{list.tagline}</h4><br />
+                  <h4>First brewed on:{list.first_brewed}</h4><br />
+                  <h4 className='side_heading'>Description:</h4>
+                  <p > {list.description}</p><br />
+                  <h4 className='side_heading'>Preparation:</h4>
                   <p>{list.brewers_tips}</p><br />
-                  <h4>Food Pairing with: </h4>
+                  <h4 className='side_heading'>Food Pairing with: </h4>
                   <li>{list.food_pairing[0]}</li>
                   <li>{list.food_pairing[1]}</li>
                   <li>{list.food_pairing[2]}</li>
